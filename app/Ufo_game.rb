362694189862fb26692643f0faf3cd44e17cc677
1-8192 <allegranzia@gmail.com>
2019-07-29 13:40:26 -0400

    made print ufo image method

--- app/Ufo_game.rb
+++ app/Ufo_game.rb
@@ -1,5 +1,4 @@
 require_relative '../lib/ufo.rb'
-require_relative '../lib/cli_functions.rb'
 
 class Ufo_game 
 
@@ -7,11 +6,29 @@ class Ufo_game
         @guesses_remaining = 6
         @wrong_guesses = []
         @correct_guesses = []
-        @secret_word = "whatever"
+        @secret_word = "testword"
+    end
+
+    def welcome
+        puts "UFO: The Game"
+        puts "instructions: save us from alien abduction by guessing        letters in the codeword."
+    end
+
+    def print_pic 
+        puts $x[6 - @guesses_remaining]
+    end
+
+    def print_status
+        puts "Incorrect Guesses:"
+        puts "NONE"
+        puts "Codeword:"
+        puts   "_ _ _ _ _ _ _ _ _ _"
     end
 
     def play
-        puts "does this work?"
+        welcome 
+        print_pic
+        print_status
     end
 
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,4 @@
 require_relative '../lib/ufo.rb'
-require_relative '../lib/cli_functions.rb'
 require_relative '../app/Ufo_game.rb'
 
 new_game = Ufo_game.new
